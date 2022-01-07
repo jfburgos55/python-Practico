b888950ce7ce07990e2206b8c5cdf1fbd0250a5d
Fredy Damián <39284784+jfburgos55@users.noreply.github.com>
2022-01-07 15:21:04 -0500

    Agregando archivo obteniendoDatosPorTeclado.py
    Agregando archivo obteniendoDatosPorTeclado.py

---
+++ obteniendoDatosPorTeclado.py
@@ -0,0 +1,15 @@
+#Autor: John Burgos
+#Fecha: 2022/01/07
+#Descripción: Programa creado para obtener datos por teclado.
+print("Introduzca su nombre: ")
+nombre=input()
+print("Introduzca su apellido: ")
+apellido=input()
+print("Hola:", nombre, apellido)
+
+print()
+print()
+
+nombre=(input("Introduzca su nombre: "))
+apellido=(input("Introduzca su apellido: "))
+print("Hola:", nombre, apellido)
